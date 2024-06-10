4d24b0ec14aa0131022caba66977db52b54f6419
이정민 <kyungheeyj45@ajou.ac.kr>
2024-06-10 15:18:43 +0900

    test : 헬스장 기구 테스트

--- src/test/java/com/HELPT/Backend/domain/gymequipment/GymEquipmentServiceTest.java
+++ src/test/java/com/HELPT/Backend/domain/gymequipment/GymEquipmentServiceTest.java
@@ -1,43 +1,46 @@
-package com.HELPT.Backend.domain.gymequipment;
-
-import com.HELPT.Backend.domain.equipment.Equipment;
-import com.HELPT.Backend.domain.equipment.EquipmentRepository;
-import com.HELPT.Backend.domain.gymequipment.dto.GymEquipmentRequest;
-import com.HELPT.Backend.domain.gymequipment.dto.GymEquipmentResponse;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
-
-@ExtendWith(MockitoExtension.class)
-class GymEquipmentServiceTest {
-
-//    private EquipmentRepository equipmentRepository;
+//package com.HELPT.Backend.domain.gymequipment;
+//
+//import com.HELPT.Backend.domain.equipment.Equipment;
+//import com.HELPT.Backend.domain.equipment.EquipmentRepository;
+//import com.HELPT.Backend.domain.gymequipment.dto.GymEquipmentRequest;
+//import com.HELPT.Backend.domain.gymequipment.dto.GymEquipmentResponse;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.DisplayName;
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.extension.ExtendWith;
+//import org.mockito.InjectMocks;
+//import org.mockito.Mock;
+//import org.mockito.junit.jupiter.MockitoExtension;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//import static org.mockito.Mockito.when;
+//
+//@ExtendWith(MockitoExtension.class)
+//class GymEquipmentServiceTest {
+//
+//    @InjectMocks
 //    private GymEquipmentService gymEquipmentService;
 //
-//    private Equipment equipment;
+//    @Mock
+//    private GymEquipmentRepository gymEquipmentRepository;
 //
-//    @BeforeEach
-//    public void setup() {
-//        equipmentRepository.deleteAll();
+//    @Mock
+//    private EquipmentRepository equipmentRepository;
 //
-//        Equipment defaultEquipment = new Equipment();
-//        defaultEquipment.setEquipmentName("Treadmill");
-//        defaultEquipment.setDefaultCount(10);
-//        defaultEquipment.setDefaultSet(3);
-//        defaultEquipment.setDefaultWeight(150);
-//
-//        equipmentRepository.save(defaultEquipment);
+//    public Equipment setUpGymEquipment(){
+//        return Equipment.builder()
+//                .equipmentId(1L)
+//                .exerciseId(100L)
+//                .equipmentName("벤치 프레스")
+//                .defaultCount(10)
+//                .defaultSet(5)
+//                .defaultWeight(25)
+//                .build();
 //    }
 //
-//
 //    @Test
-//    public void testAddGymEquipment() {
+//    @DisplayName("[Service] 기구 생성 서비스 테스트")
+//    public void saveGymEquipmentServiceTest() {
 //        GymEquipmentRequest request = new GymEquipmentRequest(/* set properties */);
 //        GymEquipment gymEquipment = request.toEntity();
 //        GymEquipment savedEquipment = new GymEquipment(/* set properties */);
@@ -51,5 +54,5 @@ class GymEquipmentServiceTest {
 //        assertEquals(savedEquipment.getId(), result.getId());
 //        // more assertions
 //    }
-
-}
+//
+//}
